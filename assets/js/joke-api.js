b3c443ad378f4c08685b06610363642f5858f7d7
denis.topallaj <denis.topallaj@student.howest.be>
2022-09-11 19:14:18 +0200

    moved joke functions to joke-api.js file

---
+++ assets/js/joke-api.js
@@ -0,0 +1,39 @@
+"use strict";
+
+async function getJokeFromAPI(racism){
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '90afc290a4msh942c639bb0fe563p15afe0jsn7d560927cf10',
+			'X-RapidAPI-Host': 'jokeapi-v2.p.rapidapi.com'
+		}
+	};
+	
+	const fetchJoke = await fetch(`${URL}${racism}`, options);
+	const res = await fetchJoke.json();
+	return res;
+}
+
+async function displayJoke(e){
+	if (!e.target.closest("#new-joke")) return;
+	const racism = checkForNSFW();
+	const joke = await getJokeFromAPI(racism);
+	const div = document.querySelector("#result");
+	div.innerHTML = "";
+	div.insertAdjacentHTML("beforeend", jokeFormPerType(joke));
+}
+
+function checkForNSFW(){
+	const optionChecked = document.querySelector("input#blacklistFlags").checked;
+	if (!optionChecked) return "";
+	return "&blacklistFlags=nsfw%2Cracist";
+}
+
+function jokeFormPerType(joke){
+	if (joke.type === "single") {
+		return `<p>${joke.joke}</p>`;
+	}
+	return `
+	<p> ${joke.setup} </p>
+	<p> ${joke.delivery} </p>`;
+}
